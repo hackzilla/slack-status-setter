e3ef60a0e958360548d1b86d9c38aac2e0803ed2
Daniel Platt <dplatt@ukwebmedia.com>
2019-12-18 09:07:24 +0000

    make compile again

--- SetStatusView.swift
+++ SetStatusView.swift
@@ -10,26 +10,72 @@
 
 import Combine
 import SwiftUI
-import URLImage
+//import URLImage
 
 struct SetStatusView: View {
     @ObservedObject private var slackController = Slack()
     @EnvironmentObject var userData: UserData
 
     let myStatus : Status
-
+    var clearStatus = [
+        "Never",
+//        "30 minutes",
+        "1 hour",
+        "4 hours",
+        "Today",
+        "This week",
+    ]
+    
+    @State private var selectedMode = 0
+     
+    
     var body: some View {
         return Form {
             Section {
-    
-            Text(myStatus.description)
-          }
+                Text(myStatus.description)
+                
+                Picker(selection: $selectedMode, label: Text("Clear after")) {
+                    ForEach(0..<clearStatus.count) {
+                        Text(self.clearStatus[$0])
+                    }
+                }
+            }
             
-            // self.slackController.setStatus(status: myStatus)
+            //
             Button(action: {
-//                guard let index = self.userData.statuses.firstIndex(of: self.editStatus) else { return }
-//                self.userData.statuses[index] = Status(emoji: self.emoji.wrappedValue, description: self.description.wrappedValue, expireHours: 0) ;
-//                self.presentationMode.wrappedValue.dismiss()
+                var expireHours : Int = 0;
+                let now = Date()
+                let calendar = Calendar.current
+                
+                switch self.selectedMode {
+                    case 1:
+                        expireHours = 1;
+                        break;
+                    case 2:
+                        expireHours = 4;
+                        break;
+                    case 3:
+                        let components = DateComponents(calendar: calendar, hour: 24)
+                        let midnight = calendar.nextDate(after: now, matching: components, matchingPolicy: .nextTime)!
+                        let diff = calendar.dateComponents([.hour], from: now, to: midnight)
+                        
+                        expireHours = diff.hour ?? 1;
+                        break;
+                    case 4:
+                        let formatter = DateFormatter()
+                        formatter.dateFormat = "EEEE"
+                  
+                        let diff = calendar.dateComponents([.hour], from: now, to: formatter.date(from: "Monday") ?? Date())
+                        
+                        expireHours = diff.hour ?? 1;
+                        break;
+
+                    default:
+                        expireHours = 0;
+                }
+                
+                let customStatus = Status(emoji: self.myStatus.emoji, description: self.myStatus.emoji, expireHours: expireHours)
+                self.slackController.setStatus(status: customStatus)
             }) {
                 Text("Set Status")
                     .foregroundColor(.blue)
